cc94393e963a8b7bfcdac25513fe5252d648e855
Constantin Dogaru <constantin.dogaru@mbntech.ro>
2021-09-21 10:22:00 +0300

    create blank smart contract

---
+++ humanity-cash-wallet/src/lib.rs
@@ -0,0 +1,39 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::near_bindgen;
+
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Wallet {
+    // SETUP CONTRACT STATE
+}
+
+#[near_bindgen]
+impl Wallet {
+    // ADD CONTRACT METHODS HERE
+}
+
+/*
+ * the rest of this file sets up unit tests
+ * to run these, the command will be:
+ * cargo test --package rust-template -- --nocapture
+ * Note: 'rust-template' comes from Cargo.toml's 'name' key
+ */
+
+// use the attribute below for unit tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::test_utils::{get_logs, VMContextBuilder};
+    use near_sdk::{testing_env};
+    use near_sdk::json_types::{ValidAccountId};
+
+    // part of writing unit tests is setting up a mock context
+    // provide a `predecessor` here, it'll modify the default context
+    fn get_context(predecessor: ValidAccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder.predecessor_account_id(predecessor);
+        builder
+    }
+
+    // TESTS HERE
+}
